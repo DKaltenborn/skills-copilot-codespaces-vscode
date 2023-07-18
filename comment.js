000f5b1ecc9de476bcec1386448f6f117c59729c
Diana Kaltenborn <45339225+DKaltenborn@users.noreply.github.com>
2023-07-18 13:18:07 +0000

    Copilot fourth commit

---
+++ comment.js
@@ -0,0 +1,7 @@
+// Create web server
+
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
